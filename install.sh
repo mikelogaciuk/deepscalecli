77fe887b883a25a80640928e8551ce66df64c980
mlog00 <michal.logaciuk@me.pl>
2023-04-06 11:59:47 +0200

    Remove install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pip install poetry
-poetry init
-mkdir -p models && mkdir -p results
-curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x2.pb --output ./models/EDSR_x2.pb
-curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x3.pb --output ./models/EDSR_x3.pb
-curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x4.pb --output ./models/EDSR_x4.pb
-poetry shell && poetry install
