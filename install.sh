3e8fb4d8ef7b6ea7924f3d4d6b0ee675ee07f2c4
mlog00 <michal.logaciuk@me.pl>
2023-04-06 11:39:19 +0200

    Final install.sh touches.

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 pip install poetry
 poetry init
-poetry shell
 poetry install
 mkdir -p models && mkdir -p results
 curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x2.pb --output ./models/EDSR_x2.pb
 curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x3.pb --output ./models/EDSR_x3.pb
 curl https://github.com/Saafke/EDSR_Tensorflow/blob/master/models/EDSR_x4.pb --output ./models/EDSR_x4.pb
+poetry shell
