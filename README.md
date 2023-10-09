4747ef1fe7f2dc42424d3a3ff48752be1b61c252
Mike Logaciuk <michal.logaciuk@hotmail.com>
2023-10-09 21:29:37 +0200

    Add docs, remove Base64 functions.

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 ## About
 
+Simple image scaller, used to scale Dalle-E images from [OpenAI's DALL-E](https://openai.com/blog/dall-e/).
+
+## Installation
+
+```bash
+mkdirp -p ~/repos && cd ~/repos && git clone https://github.com/mikelogaciuk/deepscalecli.git & cd deepscalecli
+```
+
+```bash
+poetry shell && poetry install
+```
+
+## Usage
+
+```bash
+# Scale all images from ~/Downloads/ to 4x using FSRCNN Model and save them to ~/Dalle
+deepscalecli scale fsrcnn 4 ~/Downloads/ ~/Dalle   
+```
+
+## Code
+
+This is as simple as it could be. 
+
+One function for scaling, one decorator for time measurement and one function for CLI.
+
--- deepscalecli/utils.py
+++ deepscalecli/utils.py
@@ -22,32 +22,3 @@ def chronometer(func):
 
         return result
     return wrapper
-
-
-def encode_key(key: str) -> str:
-    """
-    Encodes message to base64.
-
-    :param key: A phrase to encode
-    :return: base64 string
-    """
-    message_bytes = key.encode("ascii")
-    encode_value = base64.b64encode(message_bytes)
-    message = encode_value.decode("ascii")
-
-    return message
-
-
-def decode_key(key: str) -> str:
-    """
-    Decodes message from base64 to string.
-
-    :param key: A base64 key to decode.
-    :return: string
-    """
-    base64_bytes = key.encode('ascii')
-    message_bytes = base64.b64decode(base64_bytes)
-    message = message_bytes.decode('ascii')
-
-    return message
-
